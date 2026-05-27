6ee3be67b8fb863832d4134f0c085bf946c06428
rojanbade <rojan.bade@gmail.com>
2026-05-27 14:47:26 +0545

    initial commit

---
+++ form.js
@@ -0,0 +1,4 @@
+document.getElementById("my form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  window.location.href = "https://kuhs.edu.np";
+});
---
+++ index.js
@@ -0,0 +1,12 @@
+class User {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+function shoot() {
+  console.log("bang bang");
+  return "bang bang";
+}
+
+shoot();
